cb902f3a76f64d0602090dcd9fad0523bfdfea70
alexalotl <32069762+alexalotl@users.noreply.github.com>
2023-04-01 23:35:50 +0100

    Grabbable object

--- Trash Samurai/scripts/rubbish.gd
+++ Trash Samurai/scripts/rubbish.gd
@@ -2,27 +2,32 @@ extends RigidBody2D
 
 var type: String
 
-var isSelected: bool = false:
-	set(value):
-		if value:
-			set_freeze_enabled(true)
-		else:
-			set_freeze_enabled(false)
-			apply_central_impulse(Vector2(0,0))
+var isSelected: bool = false
 
 func _ready():
-	apply_impulse(Vector2(1000,-1000))
+	set_freeze_mode(FREEZE_MODE_STATIC)
+	apply_impulse(Vector2(300,-1000))
 	
 func _process(delta):
-	print(linear_velocity)
+	pass
 
 func _physics_process(delta):
 	if isSelected:
 		global_transform.origin = get_global_mouse_position()
-		
+	
 func _input_event(viewport, event, shape_idx):
+	print("Input Received")
 	if event is InputEventScreenTouch:
 		if event.pressed:
-			apply_impulse(Vector2(-1000,-1000))
-			isSelected = true
-
+			pickup()
+		elif isSelected:
+			drop(Input.get_last_mouse_velocity()*0.6)
+	
+func pickup():
+	set_freeze_enabled(true)
+	isSelected = true
+	
+func drop(impulse=Vector2.ZERO):
+	set_freeze_enabled(false)
+	apply_central_impulse(impulse)
+	isSelected = false
