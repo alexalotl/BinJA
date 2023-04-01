f0c3d480d9da3f53c328c91b040486be34cd4e74
alexalotl <32069762+alexalotl@users.noreply.github.com>
2023-04-01 19:54:19 +0100

    Some reorganisation

---
+++ Trash Samurai/scripts/rubbish.gd
@@ -0,0 +1,11 @@
+extends RigidBody2D
+
+var type: String
+
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
