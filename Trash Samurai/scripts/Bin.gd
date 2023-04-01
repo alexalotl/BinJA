4df5889bdb1481ba42990f02345c0fa582302dfb
Shehij Raina <sr2339@bath.ac.uk>
2023-04-01 20:59:51 +0100

    added collision detection

--- Trash Samurai/scripts/Bin.gd
+++ Trash Samurai/scripts/Bin.gd
@@ -1,4 +1,4 @@
-extends Sprite2D
+extends Area2D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -8,9 +8,12 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	look_at(get_global_mouse_position())
-	var destination = get_global_mouse_position()
-	position = destination
 	pass
-	
 
+
+
+func _on_body_entered(body):
+	if body.is_in_group("rubbish"):
+		print("hello")
+		body.free()
+	pass # Replace with function body.
