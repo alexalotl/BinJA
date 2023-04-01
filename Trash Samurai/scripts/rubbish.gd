88b94399109cca1c16fc626ff2d68d5dc1e562be
alexalotl <32069762+alexalotl@users.noreply.github.com>
2023-04-01 21:19:02 +0100

    Fixed Merge

--- Trash Samurai/scripts/rubbish.gd
+++ Trash Samurai/scripts/rubbish.gd
@@ -2,10 +2,27 @@ extends RigidBody2D
 
 var type: String
 
+var isSelected: bool = false:
+	set(value):
+		if value:
+			set_freeze_enabled(true)
+		else:
+			set_freeze_enabled(false)
+			apply_central_impulse(Vector2(0,0))
+
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	apply_impulse(Vector2(1000,-1000))
+	
 func _process(delta):
-	pass
+	print(linear_velocity)
+
+func _physics_process(delta):
+	if isSelected:
+		global_transform.origin = get_global_mouse_position()
+		
+func _input_event(viewport, event, shape_idx):
+	if event is InputEventScreenTouch:
+		if event.pressed:
+			apply_impulse(Vector2(-1000,-1000))
+			isSelected = true
+
