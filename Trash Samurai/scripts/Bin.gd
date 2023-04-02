44ec73db919f46c1df0d1e2ad325b109ab53dd54
alexalotl <32069762+alexalotl@users.noreply.github.com>
2023-04-02 03:17:17 +0100

    Game Works lol

--- Trash Samurai/scripts/Bin.gd
+++ Trash Samurai/scripts/Bin.gd
@@ -1,10 +1,27 @@
 extends Area2D
 
-@export_enum("Paper", "Food", "Plastic") var type: String
+@export_enum("Paper", "General", "Food", "Plastic", "Glass") var type: String
 
+signal success
+signal failure
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var sprite = get_node("Recycle Bin")
+	match type:
+		"Paper":
+			sprite.set_texture(load("res://assets/paper_bin.png"))
+			
+		"General":
+			sprite.set_texture(load("res://assets/general_bin.png"))
+			
+		"Food":
+			sprite.set_texture(load("res://assets/food_bin.png"))
+			
+		"Plastic":
+			sprite.set_texture(load("res://assets/plastic_bin.png"))
+			
+		"Glass":
+			sprite.set_texture(load("res://assets/glass_bin.png"))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,8 +31,12 @@ func _process(delta):
 
 
 func _on_body_entered(body):
+	print(body.type)
+	print(type)
 	if body.is_in_group("rubbish"):
+		if body.type == type:
+			success.emit()
+		else:
+			failure.emit()
 		body.free()
 		#Global.increase_score()
-		Global.increase_score()
-	pass # Replace with function body.
--- Trash Samurai/scripts/rubbish.gd
+++ Trash Samurai/scripts/rubbish.gd
@@ -1,27 +1,34 @@
 extends RigidBody2D
 
-var type: String
+@export_enum("Paper", "General", "Food", "Plastic", "Glass") var type: String
 
 var isSelected: bool = false
-
+	
 func _ready():
 	set_freeze_mode(FREEZE_MODE_STATIC)
-	apply_impulse(Vector2(300,-1000))
-	
+	var sprite = get_node("Sprite")
+	match type:
+		"Paper":
+			sprite.set_texture(load("res://assets/paper.png"))
+			
+		"General":
+			sprite.set_texture(load("res://assets/foil.png"))
+			
+		"Food":
+			sprite.set_texture(load("res://assets/banana_peel.png"))
+			
+		"Plastic":
+			sprite.set_texture(load("res://assets/cup.png"))
+			
+		"Glass":
+			sprite.set_texture(load("res://assets/cup.png"))
+
 func _process(delta):
 	pass
 
 func _physics_process(delta):
 	if isSelected:
 		global_transform.origin = get_global_mouse_position()
-	
-func _input_event(viewport, event, shape_idx):
-	print("Input Received")
-	if event is InputEventScreenTouch:
-		if event.pressed:
-			pickup()
-		elif isSelected:
-			drop(Input.get_last_mouse_velocity()*0.6)
 	
 func pickup():
 	set_freeze_enabled(true)
@@ -31,3 +38,12 @@ func drop(impulse=Vector2.ZERO):
 	set_freeze_enabled(false)
 	apply_central_impulse(impulse)
 	isSelected = false
+
+
+func _on_pickable_collider_input_event(viewport, event, shape_idx):
+	if event is InputEventScreenTouch:
+		print("Input Received")
+		if event.pressed:
+			pickup()
+		elif isSelected:
+			drop(Input.get_last_mouse_velocity())
